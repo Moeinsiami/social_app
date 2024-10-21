22b3ed959dd1271ed89e921c745871e74acb1a06
Moein Siami <moeins1216@gmail.com>
2024-10-21 21:42:13 +0330

    register UserAdmin

--- social/admin.py
+++ social/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ['username', 'email', 'phone', 'last_name']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Information', {'fields': ('date_of_birth', 'bio', 'photo', 'job', 'phone')}),
+    )
