88d8bea77e8e7a84b66c1f994b6fd617dac12b44
Moein Siami <moeins1216@gmail.com>
2024-10-24 12:31:34 +0330

    add login and register views

--- social/views.py
+++ social/views.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import *
 
-# Create your views here.
+
+def log_out(request):
+    logout(request)
+    return HttpResponse("شما خارج شدید.")
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return render(request, 'registration/register_done.html', {'user': user})
+    else:
+        form = UserRegisterForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+
+@login_required
+def edit_user(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(request.POST, instance=request.user, files=request.FILES)
+        if user_form.is_valid():
+            user_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+    context = {
+        'user_form': user_form
+    }
+    return render(request, 'registration/edit_user.html', context)
