1a586a2a9cf31ddbb1794455915d3b1cdf96b32a
Moein Siami <moeins1216@gmail.com>
2024-10-24 21:57:12 +0330

    add ticket form

--- social/forms.py
+++ social/forms.py
@@ -49,3 +49,24 @@ class UserEditForm(forms.ModelForm):
         if User.objects.exclude(id=self.instance.id).filter(username=username).exists():
             raise forms.ValidationError("username already exists!")
         return username
+
+
+class TicketForm(forms.Form):
+    SUBJECT_CHOICES = (
+        ('پیشنهاد', 'پیشنهاد'),
+        ('انتقاد', 'انتقاد'),
+        ('گزارش', 'گزارش'),
+    )
+    message = forms.CharField(widget=forms.Textarea, required=True)
+    name = forms.CharField(max_length=250, required=True)
+    email = forms.EmailField()
+    phone = forms.CharField(max_length=11, required=True)
+    subject = forms.ChoiceField(choices=SUBJECT_CHOICES)
+
+    def clean_phone(self):
+        phone = self.cleaned_data['phone']
+        if phone:
+            if not phone.isnumeric():
+                raise forms.ValidationError("شماره تلفن عددی نیست!")
+            else:
+                return phone
