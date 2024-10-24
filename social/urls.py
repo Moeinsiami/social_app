e12971a5160a8fb94dd4888783f18a50bdc43f87
Moein Siami <moeins1216@gmail.com>
2024-10-24 12:31:19 +0330

    add login and register urls

--- social/urls.py
+++ social/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from .forms import LoginForm
 
 app_name = 'social'
 
 urlpatterns = [
-    
+    path('', views.profile, name='profile'),
+    # path('login/', views.login,name="login"),
+    path('login/', auth_views.LoginView.as_view(authentication_form=LoginForm), name="login"),
+    # path('logout/', auth_views.LogoutView.as_view(), name="logout"),
+    path('logout/', views.log_out, name="logout"),
+    path('register/', views.register, name="register"),
+    path('user/edit', views.edit_user, name="edit_account"),
 ]
