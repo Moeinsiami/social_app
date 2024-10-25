cc4396b8747004c1e8c5a6356f134472e34a74aa
Moein Siami <moeins1216@gmail.com>
2024-10-25 23:01:54 +0330

    define Post model

--- social/models.py
+++ social/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+from taggit.managers import TaggableManager
 
 
 class User(AbstractUser):
@@ -8,3 +10,22 @@ class User(AbstractUser):
     photo = models.ImageField(verbose_name="تصویر", upload_to="account_images/", blank=True, null=True)
     job = models.CharField(max_length=250, verbose_name="شغل", null=True, blank=True)
     phone = models.CharField(max_length=11, null=True, blank=True)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_posts", verbose_name="نویسنده")
+    description = models.TextField(verbose_name="توضیحات")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    tags = TaggableManager()
+
+    class Meta:
+        ordering = ['-created']
+        indexes = [
+            models.Index(fields=['-created'])
+        ]
+        verbose_name = "پست"
+        verbose_name_plural = "پست ها"
+
+    def __str__(self):
+        return self.author.first_name
