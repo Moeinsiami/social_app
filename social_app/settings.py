dd527361d1ad055356d43869fd0070fa298d16eb
Moein Siami <moeins1216@gmail.com>
2024-10-25 23:02:20 +0330

    add taggit to installed apps

--- social_app/settings.py
+++ social_app/settings.py
@@ -29,14 +29,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "social.apps.SocialConfig"
+    'social.apps.SocialConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'taggit'
 ]
 
 MIDDLEWARE = [
