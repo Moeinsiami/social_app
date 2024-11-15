8b74512238016f19f664c3a56dadcac3186d6e30
Moein Siami <moeins1216@gmail.com>
2024-11-15 23:59:51 +0330

    some changes

--- social/admin.py
+++ social/admin.py
@@ -3,16 +3,55 @@ from .models import *
 from django.contrib.auth.admin import UserAdmin
 
 
+# Register your models here.
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 0
+
+
 @admin.register(User)
 class UserAdmin(UserAdmin):
-    list_display = ['username', 'email', 'phone', 'last_name']
+    list_display = ['username', 'phone', 'first_name', 'last_name']
     fieldsets = UserAdmin.fieldsets + (
         ('Additional Information', {'fields': ('date_of_birth', 'bio', 'photo', 'job', 'phone')}),
     )
 
 
+def make_deactivation(modeladmin, request, queryset):
+    reusult = queryset.update(active=False)
+    modeladmin.message_user(request, f"{reusult} Posts were rejected")
+
+
+make_deactivation.short_description = 'رد پست'
+
+
+def make_activation(modeladmin, request, queryset):
+    reusult = queryset.update(active=True)
+    modeladmin.message_user(request, f"{reusult} Posts were accepted")
+
+
+make_activation.short_description = 'تایید پست'
+
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['author', 'created']
+    list_display = ['author', 'created', 'description']
     ordering = ['created']
     search_fields = ['description']
+    inlines = [ImageInline]
+    actions = [make_deactivation, make_activation]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['post', 'name', 'created', 'active']
+    list_filter = ['active', 'created', 'updated', ]
+    search_fields = ['name', 'body']
+    list_editable = ['active']
+    # autocomplete_fields = ['post']
+
+
+admin.site.register(Contact)
+
+admin.site.register(Image)
--- social/forms.py
+++ social/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import User, Post
+from .models import User, Post, Comment
 from django.contrib.auth.forms import AuthenticationForm
 
 
@@ -19,6 +19,27 @@ class UserRegisterForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'phone']
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+            'phone': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+        }
+        labels = {
+            'username': 'نام کاربری',
+            'first_name': 'نام',
+            'last_name': 'نام خانوادگی',
+            'phone': 'شماره تماس',
+
+        }
 
     def clean_password2(self):
         cd = self.cleaned_data
@@ -29,14 +50,55 @@ class UserRegisterForm(forms.ModelForm):
     def clean_phone(self):
         phone = self.cleaned_data['phone']
         if User.objects.filter(phone=phone).exists():
-            raise forms.ValidationError('phone already exist!')
+            raise forms.ValidationError("phone already exists!")
         return phone
 
 
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'phone', 'bio', 'date_of_birth', 'job']
+        fields = ['username', 'first_name', 'last_name', 'email',
+                  'phone', 'date_of_birth', 'bio', 'photo', 'job']
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+            'phone': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+            }),
+            'date_of_birth': forms.DateInput(attrs={
+                'class': 'form-control',
+            }),
+            'bio': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+            'photo': forms.FileInput(attrs={
+                'class': 'form-control',
+            }),
+            'job': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+        }
+        labels = {
+            'username': 'نام کاربری',
+            'first_name': 'نام',
+            'last_name': 'نام خانوادگی',
+            'email': 'ایمیل',
+            'date_of_birth': 'تاریخ تولد',
+            'phone': 'شماره تماس',
+            'bio': 'بایو',
+            'job': 'شغل',
+
+        }
 
     def clean_phone(self):
         phone = self.cleaned_data['phone']
@@ -57,11 +119,14 @@ class TicketForm(forms.Form):
         ('انتقاد', 'انتقاد'),
         ('گزارش', 'گزارش'),
     )
-    message = forms.CharField(widget=forms.Textarea, required=True)
-    name = forms.CharField(max_length=250, required=True)
-    email = forms.EmailField()
-    phone = forms.CharField(max_length=11, required=True)
-    subject = forms.ChoiceField(choices=SUBJECT_CHOICES)
+    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': "3"}), required=True,
+                              label='پیام')
+    name = forms.CharField(max_length=250, widget=forms.TextInput(attrs={'class': 'form-control'}), required=True,
+                           label='نام')
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control'}), label='ایمیل')
+    phone = forms.CharField(max_length=11, widget=forms.TextInput(attrs={'class': 'form-control'}), required=True,
+                            label='شماره تماس')
+    subject = forms.ChoiceField(choices=SUBJECT_CHOICES, label='موضوع')
 
     def clean_phone(self):
         phone = self.cleaned_data['phone']
@@ -73,6 +138,30 @@ class TicketForm(forms.Form):
 
 
 class CreatePostForm(forms.ModelForm):
+    image1 = forms.ImageField(label="تصویر اول", widget=forms.FileInput(attrs={'class': 'form-control'}))
+    image2 = forms.ImageField(label="تصویر دوم", widget=forms.FileInput(attrs={'class': 'form-control'}))
+
     class Meta:
         model = Post
-        fields = ['tags', 'description']
+        fields = ['description', 'tags']
+        widgets = {
+            'description': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': "3"
+            }),
+        }
+        labels = {
+            'tags': 'تگ ها',
+        }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['body']
+        widgets = {
+            'body': forms.TextInput(attrs={
+                'placeholder': 'کامنت بزارید...',
+                'class': 'form-control input-sm'
+            }),
+        }
